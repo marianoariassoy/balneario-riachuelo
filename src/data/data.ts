c1e4593ff22e26dc1a6fde4505a2d36f6fbaeda0
marianoariassoy <hola@marianoarias.soy>
2023-09-12 15:39:29 -0300

    add updates

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,4 +1,4 @@
-export const data = [
+export const slider1 = [
   {
     id: 1,
     image: '/images/home1.webp'
@@ -12,7 +12,23 @@ export const data = [
     image: '/images/home3.webp'
   },
   {
+    id: 5,
+    image: '/images/home5.webp'
+  },
+  { id: 6, image: '/images/home6.webp' },
+  {
     id: 4,
     image: '/images/home4.webp'
+  }
+]
+
+export const slider2 = [
+  {
+    id: 1,
+    image: '/images/plano1.webp'
+  },
+  {
+    id: 2,
+    image: '/images/plano2.webp'
   }
 ]
